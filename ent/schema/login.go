7082db6b6f635e082f6fed478b57fca577353072
derFinlay <ibimseinskatze@gmail.com>
2024-01-10 23:37:41 +0100

    fix(schema): added last login time

--- ent/schema/login.go
+++ ent/schema/login.go
@@ -16,6 +16,7 @@ func (Login) Fields() []ent.Field {
 	return []ent.Field{
 		field.String("username").NotEmpty().Unique(),
 		field.String("password").NotEmpty(),
+		field.Time("last_login"),
 		field.Time("created_at").Default(time.Now).Immutable(),
 		field.Time("updated_at").Default(time.Now).UpdateDefault(time.Now),
 	}
--- ent/schema/user.go
+++ ent/schema/user.go
@@ -15,7 +15,8 @@ func (User) Fields() []ent.Field {
 	return []ent.Field{
 		field.String("name").NotEmpty().Unique().MaxLen(20),
 		field.String("username").Unique(),
-		field.String("password"),
+		field.String("password").NotEmpty(),
+		field.Time("last_login"),
 		field.Time("created_at").Default(time.Now).Immutable(),
 		field.Time("updated_at").Default(time.Now).UpdateDefault(time.Now),
 	}
