94c21c2ac845ef6297c07144e7e85bc59bb06d7b
derFinlay <ibimseinskatze@gmail.com>
2024-01-11 22:10:26 +0100

    Update order schema with default status field value

--- ent/schema/order.go
+++ ent/schema/order.go
@@ -14,7 +14,7 @@ type Order struct {
 
 func (Order) Fields() []ent.Field {
 	return []ent.Field{
-		field.String("test"),
+		field.String("status").Default("pending"),
 		field.Time("created_at").Default(time.Now).Immutable(),
 		field.Time("updated_at").Default(time.Now).UpdateDefault(time.Now),
 	}
